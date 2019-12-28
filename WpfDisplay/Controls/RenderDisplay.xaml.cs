f151251e9984a2b0710c93e118ca6646dc739561
bezo97 <bezozo97@gmail.com>
2019-12-28 14:18:36 +0100

    remove old fps counter

--- WpfDisplay/Controls/RenderDisplay.xaml.cs
+++ WpfDisplay/Controls/RenderDisplay.xaml.cs
@@ -31,8 +31,6 @@ namespace WpfDisplay.Controls
     {
         public RendererGL Renderer { get; private set; }
 
-        public int FPS { get; set; }
-
         private GLControl display1;
         private KeyboardController kbc;
 
@@ -93,15 +91,10 @@ namespace WpfDisplay.Controls
             return (Size)transformToDevice.Transform((Vector)element.DesiredSize);
         }
         
-        Stopwatch fpsCounter = new Stopwatch();
         private void R_DisplayFrameCompleted(object sender, EventArgs e)
         {
-            ctx.MakeCurrent(display1.WindowInfo);
+            //ctx.MakeCurrent(display1.WindowInfo);
             ctx.SwapBuffers();
-            FPS = (int)(fpsCounter.ElapsedMilliseconds > 0 ? 1000 / (fpsCounter.ElapsedMilliseconds) : 0);
-            fpsCounter.Stop();
-            fpsCounter.Restart();
-            //TODO: update fps view: NotifyPropertyChanged mashol, vagy itt Dispatcher.Invoke(()=>{ ... });
         }
 
         float lastX;
