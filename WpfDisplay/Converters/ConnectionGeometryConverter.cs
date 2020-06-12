f659a412cc52857aeaa1e181bfd487d4612e3af9
bezo97 <bezozo97@gmail.com>
2020-06-12 14:35:52 +0200

    clean & refactor: connection viewmodel and view xaml

---
+++ WpfDisplay/Converters/ConnectionGeometryConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfDisplay.Converters
+{
+    public class ConnectionGeometryConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            PathGeometry g1 = (PathGeometry)values[0];
+            EllipseGeometry g2 = (EllipseGeometry)values[1];
+            bool isLoopbackConnection = (bool)values[2];//parameter;
+            if (isLoopbackConnection)
+                return g2;
+            else
+                return g1;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfDisplay/ViewModels/ConnectionViewModel.cs
+++ WpfDisplay/ViewModels/ConnectionViewModel.cs
@@ -10,16 +10,24 @@ using System.Windows.Media;
 
 namespace WpfDisplay.ViewModels
 {
-    //TODO: cleanup & refactor
     public class ConnectionViewModel : ObservableObject
     {
 
         public readonly IteratorViewModel from;
         public readonly IteratorViewModel to;//TODO: replace with toPoint
 
-        public Geometry ArrowBody { get; private set; }
-        public Geometry ArrowHeadLeft { get; private set; }
-        public Geometry ArrowHeadRight { get; private set; }
+        public Point StartPoint => new Point(from.XCoord, from.YCoord);
+        public Point EndPoint => new Point(to.XCoord, to.YCoord);
+        public Point ArrowHeadMid { get; set; }
+        public Point ArrowHeadLeft { get; set; }
+        public Point ArrowHeadRight { get; set; }
+        public PointCollection BodyPoints { get; set; } //not observable??
+
+        public bool IsLoopback => (from == to);
+        public double EllipseRadius { get; set; }
+        public Point EllipseMid { get; set; }
+
+        public double Weight => from.iterator.WeightTo[to.iterator];
 
         public ConnectionViewModel(IteratorViewModel from, IteratorViewModel to)
         {
@@ -40,13 +48,19 @@ namespace WpfDisplay.ViewModels
 
         public void UpdateGeometry()
         {
-            if (from == to)
+            if (IsLoopback)
                 CalcGeometryToSelf();
             else
                 CalcGeometryByPoints();
-            RaisePropertyChanged(() => ArrowBody);
+
+            RaisePropertyChanged(() => ArrowHeadMid);
             RaisePropertyChanged(() => ArrowHeadLeft);
             RaisePropertyChanged(() => ArrowHeadRight);
+            RaisePropertyChanged(() => StartPoint);
+            RaisePropertyChanged(() => EndPoint);
+            RaisePropertyChanged(() => BodyPoints);
+            RaisePropertyChanged(() => EllipseMid);
+            RaisePropertyChanged(() => EllipseRadius);
         }
 
         private void CalcGeometryByPoints()
@@ -58,36 +72,30 @@ namespace WpfDisplay.ViewModels
             double ydir = p2.Y - p1.Y;
             double angle = Math.Atan2(ydir, xdir) + Math.PI / 4;//TODO: make this a setting?
 
-            PathSegmentCollection seg = new PathSegmentCollection(1);
-            seg.Add(new PolyBezierSegment(new PointCollection(3) {
-                new Point((p1.X * 2 + p2.X) / 3 + 30*Math.Cos(angle), (p1.Y * 2 + p2.Y) / 3 + 30*Math.Sin(angle)),
-                new Point((p1.X + p2.X * 2) / 3 + 30*Math.Cos(angle), (p1.Y + p2.Y * 2) / 3 + 30*Math.Sin(angle)), p2 }, true));
-
-            ArrowBody = new PathGeometry(new PathFigureCollection { new PathFigure(p1, seg, false) });
-            
-            //Find middle of the arrow body. TODO: find better way
-            PathGeometry flattened = ArrowBody.GetFlattenedPathGeometry();//bezier -> line path
-            double minL = 9999;
-            PointCollection ffig = ((PolyLineSegment)flattened.Figures[0].Segments[0]).Points;
-            double halfX = (ffig[0].X + ffig[ffig.Count - 1].X) / 2;
-            double halfY = (ffig[0].Y + ffig[ffig.Count - 1].Y) / 2;
-            int iP = 0;
-            for (; iP < ffig.Count; iP++)
+            BodyPoints = new PointCollection(3)
             {
-                double nextL = Math.Min(minL, Math.Sqrt(Math.Pow(halfX - ffig[iP].X, 2) + Math.Pow(halfY - ffig[iP].Y, 2)));
-                if (nextL < minL)
-                    minL = nextL;
-                else
-                    break;
-            }
+                new Point(
+                    (p1.X * 2 + p2.X) / 3 + 30 * Math.Cos(angle),
+                    (p1.Y * 2 + p2.Y) / 3 + 30 * Math.Sin(angle)),
+                new Point(
+                    (p1.X + p2.X * 2) / 3 + 30 * Math.Cos(angle),
+                    (p1.Y + p2.Y * 2) / 3 + 30 * Math.Sin(angle)),
+                p2
+            };
 
-            //calc arrow head
-            Point mid = ffig[iP];
-            Point prev = ffig[iP - 1];
-            Point dir = new Point(mid.X - prev.X, mid.Y - prev.Y);
+            Point mid = new Point(
+                (p1.X + p2.X) / 2 + 22 * Math.Cos(angle),
+                (p1.Y + p2.Y) / 2 + 22 * Math.Sin(angle));
+            Point dir = new Point(p2.X - p1.X, p2.Y - p1.Y);
             angle = Math.Atan2(dir.Y, dir.X);
-            ArrowHeadLeft = new LineGeometry(mid, new Point(mid.X - Math.Cos(angle + 0.5) * IteratorViewModel.BaseSize / 5.0, mid.Y - Math.Sin(angle + 0.5) * IteratorViewModel.BaseSize / 5.0));
-            ArrowHeadRight = new LineGeometry(mid, new Point(mid.X - Math.Cos(angle - 0.5) * IteratorViewModel.BaseSize / 5.0, mid.Y - Math.Sin(angle - 0.5) * IteratorViewModel.BaseSize / 5.0));
+
+            ArrowHeadMid = mid;
+            ArrowHeadLeft = new Point(
+                mid.X - Math.Cos(angle + 0.5) * IteratorViewModel.BaseSize / 5.0, 
+                mid.Y - Math.Sin(angle + 0.5) * IteratorViewModel.BaseSize / 5.0);
+            ArrowHeadRight = new Point(
+                mid.X - Math.Cos(angle - 0.5) * IteratorViewModel.BaseSize / 5.0, 
+                mid.Y - Math.Sin(angle - 0.5) * IteratorViewModel.BaseSize / 5.0);
         }
 
         private void CalcGeometryToSelf()
@@ -108,16 +116,22 @@ namespace WpfDisplay.ViewModels
             double r = from.WeightedSize / 2.0 / 5.0 * 4.0;
             double cosa = Math.Cos(loopbackAngle);
             double sina = Math.Sin(loopbackAngle);
-            Point mid = new Point(from.XCoord + r * cosa, from.YCoord + r * sina);
-            ArrowBody = new EllipseGeometry(mid, r, r);
-            mid = new Point(from.XCoord + 2 * r * cosa, from.YCoord + 2 * r * sina);
+            Point emid = new Point(from.XCoord + r * cosa, from.YCoord + r * sina);
+            EllipseMid = emid;
+            EllipseRadius = r;
+
+            Point mid = new Point(from.XCoord + 2 * r * cosa, from.YCoord + 2 * r * sina);
             double a = Math.Atan2(sina, cosa) - 3.1415 / 4.0;
             cosa = Math.Cos(a);
             sina = Math.Sin(a);
-            ArrowHeadLeft = new LineGeometry(mid, new Point(mid.X - IteratorViewModel.BaseSize / 5.0 * cosa, mid.Y - IteratorViewModel.BaseSize / 5.0 * sina));
-            ArrowHeadRight = new LineGeometry(mid, new Point(mid.X - IteratorViewModel.BaseSize / 5.0 * sina, mid.Y + IteratorViewModel.BaseSize / 5.0 * cosa));
+            ArrowHeadMid = mid;
+            ArrowHeadLeft = new Point(
+                mid.X - IteratorViewModel.BaseSize / 5.0 * cosa,
+                mid.Y - IteratorViewModel.BaseSize / 5.0 * sina);
+            ArrowHeadRight = new Point(
+                mid.X - IteratorViewModel.BaseSize / 5.0 * sina,
+                mid.Y + IteratorViewModel.BaseSize / 5.0 * cosa);
         }
 
-        public double Weight => from.iterator.WeightTo[to.iterator];
     }
 }
