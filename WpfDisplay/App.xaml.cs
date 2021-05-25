a7b4f95cb5d713e43e6cbc3673d9f2666c7e0524
bezo97 <bezozo97@gmail.com>
2021-05-26 01:56:12 +0200

    NvOptimus helper

--- WpfDisplay/App.xaml.cs
+++ WpfDisplay/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfDisplay.Helper;
 
 namespace WpfDisplay
 {
@@ -13,5 +14,9 @@ namespace WpfDisplay
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            NvOptimusHelper.InitializeDedicatedGraphics();
+        }
     }
 }
