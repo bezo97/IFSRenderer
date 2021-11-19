025462054506ffd71bde76e37a2b44579a709ce9
bezo97 <bezozo97@gmail.com>
2021-11-19 15:15:41 +0100

    use shift, alt keys to modify the increment size

--- WpfDisplay/Views/ValueSlider.xaml.cs
+++ WpfDisplay/Views/ValueSlider.xaml.cs
@@ -76,6 +76,10 @@ namespace WpfDisplay.Views
         public static readonly DependencyProperty MaxValueProperty =
             DependencyProperty.Register("MaxValue", typeof(double?), typeof(ValueSlider), new PropertyMetadata(null));
 
+        private static double IncrementMultiplier => 1.0
+            * (Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt) ? 10 : 1)
+            * (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift) ? 0.1 : 1);
+
         public ValueSlider()
         {
             InitializeComponent();
@@ -84,13 +88,13 @@ namespace WpfDisplay.Views
         private void Down_Click(object sender, RoutedEventArgs e)
         {
             ValueChangedCommand?.Execute(null);
-            Value = Math.Round(Value - Increment, 3);
+            Value = Math.Round(Value - Increment * IncrementMultiplier, 3);
         }
 
         private void Up_Click(object sender, RoutedEventArgs e)
         {
             ValueChangedCommand?.Execute(null);
-            Value = Math.Round(Value + Increment, 3);
+            Value = Math.Round(Value + Increment * IncrementMultiplier, 3);
         }
 
         private void Animate_Click(object sender, RoutedEventArgs e)
@@ -140,7 +144,7 @@ namespace WpfDisplay.Views
             if (e.LeftButton == MouseButtonState.Pressed && displayLabel.IsMouseCaptured)
             {
                 double delta = (e.GetPosition(Window.GetWindow(this)).X - dragp.X);
-                Value = lastv + delta * Increment;
+                Value = lastv + delta * Increment * IncrementMultiplier;
 
                 //reset position on mouseleave
                 if (VisualTreeHelper.HitTest(this, e.GetPosition(displayLabel)) == null)
