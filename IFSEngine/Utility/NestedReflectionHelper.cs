29f078b15fd034b817eff895a752a470244ff111
bezo97 <bezozo97@gmail.com>
2022-05-07 15:36:48 +0200

    support animating value-types (Vector3, Quaternion, ..)

--- IFSEngine/Utility/NestedReflectionHelper.cs
+++ IFSEngine/Utility/NestedReflectionHelper.cs
@@ -13,11 +13,16 @@ internal static class NestedReflectionHelper
 {
     /// <param name="obj">The root object that the path applies to.</param>
     /// <param name="memberPath">A path to a property or field under the root object. Such as "[0].MyDict.[asd]".</param>
-    /// <param name="newValue">The new value to be set for the spceified member.</param>
+    /// <param name="newValue">The new value to be set for the specified member.</param>
     public static void SetMemberValueByPath(object obj, string memberPath, object newValue)
     {
         var pathSegments = new Queue<string>(memberPath.Split('.'));
-        while(pathSegments.TryDequeue(out var segment))
+        _ = SetRecursive(obj, pathSegments, newValue);
+    }
+
+    private static object SetRecursive(object obj, Queue<string> pathSegments, object newValue)
+    {
+        if (pathSegments.TryDequeue(out var segment))
         {
             ParseMemberPathSegment(segment, out var memberName, out var index);
 
@@ -25,21 +30,30 @@ internal static class NestedReflectionHelper
             if (propInfo is not null)
             {//member is a property
                 if (pathSegments.Count > 0)
-                    obj = propInfo.GetValue(obj, index)!;
+                {
+                    var propValue = propInfo.GetValue(obj, index)!;
+                    propValue = SetRecursive(propValue, pathSegments, newValue);
+                    if(propValue.GetType().IsValueType)
+                        propInfo.SetValue(obj, propValue, index);
+                }
                 else
                     propInfo.SetValue(obj, newValue, index);
             }
             else
             {//member is a field
                 var fieldInfo = obj.GetType().GetField(memberName)!;
-                TypedReference reference = __makeref(obj);
-                
                 if (pathSegments.Count > 0)
-                    obj = fieldInfo.GetValue(obj)!;
+                {
+                    var fieldValue = fieldInfo.GetValue(obj)!;
+                    fieldValue = SetRecursive(fieldValue, pathSegments, newValue);
+                    if (fieldValue.GetType().IsValueType)
+                        fieldInfo.SetValue(obj, fieldValue);
+                }
                 else
                     fieldInfo.SetValue(obj, Convert.ToSingle(newValue));//hack: cast double to float
             }
         }
+        return obj;
     }
 
     private static void ParseMemberPathSegment(string segment, out string memberName, out object?[]? index)
