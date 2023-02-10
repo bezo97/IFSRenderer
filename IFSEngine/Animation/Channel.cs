b1e324d72bdf0d9a3f6665c4dacbcfe4f039bc65
bezo97 <bezozo97@gmail.com>
2023-02-10 23:54:12 +0100

    AudioChannelDriver applies on channels with single keyframe

--- IFSEngine/Animation/Channel.cs
+++ IFSEngine/Animation/Channel.cs
@@ -1,11 +1,8 @@
 ﻿#nullable enable
 using IFSEngine.Animation.ChannelDrivers;
-using IFSEngine.Utility;
-using OpenTK;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace IFSEngine.Animation;
 
@@ -27,31 +24,34 @@ public class Channel
     {
         var keyframes = Keyframes.OrderBy(k => k.t).ToList();
 
+        double eval = 0;
         if (t >= keyframes.Last().t)
-            return keyframes.Last().Value;
-        if (t <= keyframes.First().t)
-            return keyframes.First().Value;
+            eval = keyframes.Last().Value;
+        else if (t <= keyframes.First().t)
+            eval = keyframes.First().Value;
+        else
+        {//between keyframes
+            var previousKeyframe = keyframes.Where(c => c.t < t).MaxBy(c => c.t)!;
+            var nextKeyframe = keyframes.Where(c => c.t > t).MinBy(c => c.t)!;
 
-        var previousKeyframe = keyframes.Where(c => c.t < t).MaxBy(c => c.t)!;
-        var nextKeyframe = keyframes.Where(c => c.t > t).MinBy(c => c.t)!;
+            var tNorm = (t - previousKeyframe.t) / (nextKeyframe.t - previousKeyframe.t);
 
-        var tNorm = (t - previousKeyframe.t) / (nextKeyframe.t - previousKeyframe.t);
+            //easing
+            double tEasing = previousKeyframe.EasingDirection switch
+            {
+                EasingDirection.In => Math.Pow(tNorm, previousKeyframe.EasingPower),
+                EasingDirection.Out => 1.0 - Math.Pow(tNorm, previousKeyframe.EasingPower),
+                EasingDirection.InOut => InOutPowerEasing(tNorm, previousKeyframe.EasingPower, nextKeyframe.EasingPower),
+                _ => throw new NotImplementedException(),
+            };
 
-        //easing
-        double tEasing = previousKeyframe.EasingDirection switch
-        {
-            EasingDirection.In => Math.Pow(tNorm, previousKeyframe.EasingPower),
-            EasingDirection.Out => 1.0 - Math.Pow(tNorm, previousKeyframe.EasingPower),
-            EasingDirection.InOut => InOutPowerEasing(tNorm, previousKeyframe.EasingPower, nextKeyframe.EasingPower),
-            _ => throw new NotImplementedException(),
-        };
+            //interpolation
+            var p0 = keyframes.ElementAtOrDefault(keyframes.IndexOf(previousKeyframe) - 1) ?? previousKeyframe;
+            var p3 = keyframes.ElementAtOrDefault(keyframes.IndexOf(nextKeyframe) + 1) ?? nextKeyframe;
+            eval = CatmullRom(p0.Value, previousKeyframe.Value, nextKeyframe.Value, p3.Value, tEasing);
+        }
 
-        //interpolation
-        var p0 = keyframes.ElementAtOrDefault(keyframes.IndexOf(previousKeyframe) - 1) ?? previousKeyframe;
-        var p3 = keyframes.ElementAtOrDefault(keyframes.IndexOf(nextKeyframe) + 1) ?? nextKeyframe;
-        double eval = CatmullRom(p0.Value, previousKeyframe.Value, nextKeyframe.Value, p3.Value, tEasing);
-
-        if(AudioChannelDriver is not null)
+        if (AudioChannelDriver is not null)
         {
             eval = AudioChannelDriver.Apply(eval, t);
         }
@@ -61,7 +61,7 @@ public class Channel
 
     private static double InOutPowerEasing(double t, double pow1, double pow2)
     {
-        if (t < 0.5) 
+        if (t < 0.5)
             return Math.Pow(t * 2.0, pow1) / 2.0;
         return 1.0 - Math.Pow((1.0 - t) * 2.0, pow2) / 2.0;
     }
