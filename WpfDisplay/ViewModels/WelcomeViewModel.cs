cda657e56eda824daa2af8aa4252f8d375116fb7
bezo97 <bezozo97@gmail.com>
2023-10-11 00:13:11 +0200

    add welcome window initial markup

---
+++ WpfDisplay/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDisplay.ViewModels;
+
+public sealed partial class WelcomeViewModel : ObservableObject
+{
+
+}
